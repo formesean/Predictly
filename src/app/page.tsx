10e1cf364e04eb7b644f0a7810e26090c88e5ed3
formesean <seanaguilar698@gmail.com>
2024-12-01 23:31:22 +0800

    refactor: component styling and grid placements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import { ProjectionChart } from "../components/ProjectionChart";
 import Papa from "papaparse";
 import { financialProjection } from "../lib/lagrange";
 import { HistoricalData, ProjectionResult } from "@/types/types";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { FadeText } from "@/components/ui/fade-text";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
     const [projectionYear, setProjectionYear] = useState<number>(2024);
@@ -112,27 +115,79 @@ export default function Home() {
     };
 
     return (
-        <main className="mx-auto flex flex-col items-center gap-6 p-3 md:p-6 max-w-7xl w-full">
-            <FileUploadProjection
-                onFileUpload={handleFileUpload}
-                projectionYear={projectionYear}
-                setProjectionYear={setProjectionYear}
-                handleProjection={handleProjection}
-                error={error}
-            />
+        <main className="mx-auto flex flex-col items-center gap-6 p-4 md:p-6 max-w-7xl w-full">
+            <section className="flex flex-col pt-6 gap-4">
+                <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+                    <FadeText
+                        className="text-3xl lg:text-4xl font-bold"
+                        direction="left"
+                        framerProps={{
+                            show: { transition: { delay: 0.8 } },
+                        }}
+                        text="Predictly:"
+                    />
+                    <FadeText
+                        className="text-3xl lg:text-4xl font-bold"
+                        direction="down"
+                        framerProps={{
+                            show: { transition: { delay: 1.2 } },
+                        }}
+                        text="Smarter Financial Projections"
+                    />
+                </div>
+                <div className="text-justify mx-auto max-w-[940px]">
+                    <FadeText
+                        className="text-base lg:text-lg text-muted-foreground"
+                        direction="right"
+                        framerProps={{
+                            show: { transition: { delay: 1.6 } },
+                        }}
+                        text="Your go-to financial projection tool, leveraging the
+                    Lagrange Method and historical data to deliver accurate
+                    revenue and performance forecasts. Designed for business
+                    owners, project managers, and financial analysts, Predictly
+                    empowers you to make smarter, data-driven decisions with
+                    confidence and ease."
+                    />
+                </div>
+            </section>
 
-            <section className="flex flex-col lg:flex-row justify-center gap-6 lg:gap-10 w-full">
-                <HistoricalDataTable
-                    historicalData={historicalData}
-                    handleDataUpdate={handleDataUpdate}
-                    handleAddRow={handleAddRow}
-                    handleDeleteRow={handleDeleteRow}
-                    formatRevenue={formatRevenue}
-                />
-                <ProjectionChart
-                    chartData={chartData}
-                    chartConfig={chartConfig}
-                />
+            <Separator />
+
+            <section className="pt-4 flex flex-col lg:flex-row items-start justify-center w-full gap-6">
+                <div className="flex flex-col lg:flex-row justify-center gap-4 lg:gap-6 w-full">
+                    <FileUploadProjection
+                        onFileUpload={handleFileUpload}
+                        projectionYear={projectionYear}
+                        setProjectionYear={setProjectionYear}
+                        handleProjection={handleProjection}
+                        error={error}
+                    />
+                </div>
+
+                <div className="flex flex-col lg:flex-row justify-center gap-4 lg:gap-6 w-full">
+                    <Tabs defaultValue="chart" className="w-auto">
+                        <TabsList className="grid w-full grid-cols-2">
+                            <TabsTrigger value="table">Table</TabsTrigger>
+                            <TabsTrigger value="chart">Chart</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="table">
+                            <HistoricalDataTable
+                                historicalData={historicalData}
+                                handleDataUpdate={handleDataUpdate}
+                                handleAddRow={handleAddRow}
+                                handleDeleteRow={handleDeleteRow}
+                                formatRevenue={formatRevenue}
+                            />
+                        </TabsContent>
+                        <TabsContent value="chart">
+                            <ProjectionChart
+                                chartData={chartData}
+                                chartConfig={chartConfig}
+                            />
+                        </TabsContent>
+                    </Tabs>
+                </div>
             </section>
         </main>
     );
--- src/components/FileUploadProjection.tsx
+++ src/components/FileUploadProjection.tsx
@@ -1,6 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import RippleButton from "./ui/ripple-button";
 
 type FileUploadProjectionProps = {
     onFileUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -17,39 +18,39 @@ export const FileUploadProjection = ({
     handleProjection,
     error,
 }: FileUploadProjectionProps) => (
-    <section className="flex flex-col gap-4 md:gap-6 w-full mx-auto">
-        <div className="flex flex-col gap-4 md:gap-6">
-            <div className="flex flex-col sm:flex-row justify-center items-start sm:items-center gap-4 sm:gap-10 w-full pt-6">
-                <div className="flex flex-col gap-2 w-full sm:w-auto">
-                    <Label htmlFor="csv-file">Upload CSV File</Label>
-                    <Input
-                        id="csv-file"
-                        type="file"
-                        accept=".csv"
-                        onChange={onFileUpload}
-                    />
-                </div>
-
-                <div className="flex flex-col gap-2 w-full sm:w-auto">
-                    <Label htmlFor="projection-year">Projection Year:</Label>
-                    <Input
-                        id="projection-year"
-                        type="number"
-                        value={projectionYear || ""}
-                        onChange={(e) =>
-                            setProjectionYear(Number(e.target.value))
-                        }
-                        placeholder="Enter projection year"
-                    />
-                </div>
+    <div className="flex flex-col gap-4 md:gap-6 h-full justify-center">
+        <div className="flex flex-col sm:flex-row justify-center items-start sm:items-center gap-4 sm:gap-10 w-full">
+            <div className="flex flex-col gap-2 w-full">
+                <Label htmlFor="csv-file">Upload CSV File</Label>
+                <Input
+                    id="csv-file"
+                    type="file"
+                    accept=".csv"
+                    onChange={onFileUpload}
+                />
             </div>
 
-            <div className="flex w-full">
-                <Button onClick={handleProjection} className="flex-grow">
-                    Calculate Projection
-                </Button>
-                {error && <p className="text-red-500 text-sm">{error}</p>}
+            <div className="flex flex-col gap-2 w-full">
+                <Label htmlFor="projection-year">Projection Year:</Label>
+                <Input
+                    id="projection-year"
+                    type="number"
+                    value={projectionYear || ""}
+                    onChange={(e) => setProjectionYear(Number(e.target.value))}
+                    placeholder="Enter projection year"
+                />
             </div>
         </div>
-    </section>
+
+        <div className="flex flex-col gap-2 w-full">
+            <RippleButton
+                onClick={handleProjection}
+                className="flex-grow"
+                rippleColor="#ADD8E6"
+            >
+                Calculate Projection
+            </RippleButton>
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+        </div>
+    </div>
 );
